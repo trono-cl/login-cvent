8c4b5841043b169f5176cc2962dcd0228e0ac435
cecv12 <cecv12@gmail.com>
2020-10-11 17:39:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,13 +9,19 @@
     <h1>Formulario</h1>
     <form action="datos.php" method="POST">
         <label for="AccountNumber"> Numero de cuenta: </label>
-        <input type="text" name="AccountNumber"><br><br>
+        <input type="text" name="AccountNumber" value="CVENTCS714"><br><br>
 
         <label for="UserName"> Nombre: </label>
-        <input type="text" name="UserName"><br><br>
+        <input type="text" name="UserName" value="CVENTCS714API" ><br><br>
         
         <label for="Password"> Contraseña:</label>
-        <input type="text" name="Password"><br><br>
+        <input type="text" name="Password" value="Redalert1."><br><br>
+
+        <label for="StartDate"> StartDate:</label>
+        <input type="text" name="StartDate" value="2020-10-07T00:00:00"><br><br>
+
+        <label for="EndDate"> EndDate:</label>
+        <input type="text" name="EndDate" value="2020-10-08T23:59:00"><br><br>
 
         <input type="submit" value="Enviar">
     </form>
