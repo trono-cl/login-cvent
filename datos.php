564c1d3118ec9d8ce1ba75bf99f3edb9f550d205
cecv12 <cecv12@gmail.com>
2020-10-11 17:38:54 -0300

    Update datos.php

---
+++ datos.php
@@ -0,0 +1,77 @@
+<?php
+
+$AccountNumber = $_POST['AccountNumber'];
+$UserName = $_POST['UserName'];
+$Password = $_POST['Password'];
+$StartDate = $_POST['StartDate'];
+$EndDate = $_POST['EndDate'];
+
+$client = new SoapClient("https://api.cvent.com/soap/V200611.ASMX?WSDL", array('trace' => true, 'exceptions' => true));
+
+$params = array();
+$params['AccountNumber']    = $AccountNumber;
+$params['UserName']    = $UserName;
+$params['Password']    = $Password;
+
+$response = $client->Login($params);
+
+$arr_response = array();
+$arr_response[] = $response->LoginResult->ServerURL;
+$arr_response[] = $response->LoginResult->LoginSuccess;
+$arr_response[] = $response->LoginResult->CventSessionHeader;
+
+$ServerURL = $arr_response[0];
+$LoginSuccess = $arr_response[1];
+$CventSessionHeader = $arr_response[2];
+
+echo "CventClient:" . "<br />";
+echo "ServerURL: " . $ServerURL . "<br />";
+echo "LogginSuccess: " . $LoginSuccess . "<br />";
+echo "CventSessionHeader: " . $CventSessionHeader.'<br/>' ;
+
+//fin de llamado del login
+//se setea cabecera con el CventSessionHeader
+$client->__setLocation($ServerURL);
+$header_body = array('CventSessionValue' => $CventSessionHeader);
+$header = new SoapHeader('http://api.cvent.com/2006-11', 'CventSessionHeader', $header_body);
+$client->__setSoapHeaders($header);
+
+//Se realiza llamado al metodo get update
+$paramsGetUpdated = array();
+$paramsGetUpdated['ObjectType'] = "Registration";
+$paramsGetUpdated['StartDate'] = $StartDate;
+$paramsGetUpdated['EndDate'] = $EndDate;
+
+$responseGetUpdated = $client->GetUpdated($paramsGetUpdated);
+
+$arr_responseGetUpdated = array();
+$arr_responseGetUpdated[] = $responseGetUpdated->GetUpdatedResult->Id;
+
+$id = $arr_responseGetUpdated[0];
+
+echo "getUpdate ID: ";
+echo '<br/>';
+print_r($id);
+
+//LLamado a retrieve
+$paramsRetrieve = array();
+$paramsRetrieve['ObjectType']   = "Registration";
+$paramsRetrieve['Ids']          = $id;
+
+$responseRetrieve  = $client->Retrieve($paramsRetrieve);
+
+$arr_responseRetrieve = array();
+$arr_responseRetrieve[] = $responseRetrieve->RetrieveResult;
+echo '<br/>';
+
+$CvObject = $arr_responseRetrieve[0];
+
+echo "RESPONSE:\n" . $client->__getLastResponse() . "\n";
+
+echo '<br/>';
+echo "Retrieve ID: ";
+echo '<br/>';
+print_r($CvObject);
+
+
+?>
