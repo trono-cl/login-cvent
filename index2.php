2eabe1609d2a3995a47931f3a41b8439dc79e7bb
cecv12 <cecv12@gmail.com>
2020-10-13 00:50:19 -0300

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de Prueba</title>
+</head>
+<body>
+    <h1>Formulario</h1>
+    <form action="datos2.php" method="POST">
+    
+        <label for="emaiul">email: </label>
+        <input type="text" name="email" value=""><br><br>
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
